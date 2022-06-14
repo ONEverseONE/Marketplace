e2c41a1ec3d03ca7c23a05477bd50f600809efa7
Ace <51465755+AceVikings@users.noreply.github.com>
2022-06-14 21:57:34 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+
 const dotenv = require("dotenv");
-dotenv.config({path: __dirname + '/.env'});
+dotenv.config({ path: __dirname + "/.env" });
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -18,18 +20,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- module.exports = {
-  solidity: "0.8.7",
-  networks:{
-    harmonytestnet:{
-      url:"https://api.s0.b.hmny.io",
-      accounts: [`${process.env.PRIVATE_KEY}`]
-    }
+module.exports = {
+  solidity: "0.8.12",
+  networks: {
+    harmonytestnet: {
+      url: "https://api.s0.b.hmny.io",
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    harmony: {
+      url: "https://api.harmony.one",
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
   },
   etherscan: {
     apiKey: {
       harmonyTest: `${process.env.ETHERSCAN_KEY}`,
-    }
-  }
+      harmony: `${process.env.ETHERSCAN_KEY}`,
+    },
+  },
 };
-
